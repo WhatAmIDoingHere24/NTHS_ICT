5642152c1e27aebd1434f547400752d1ac8b0542
Brayden <braydencunningham28@gmail.com>
2025-04-01 21:48:19 -0800

    Created GUI Window

--- GUI.py
+++ GUI.py
@@ -1,1 +1,17 @@
+import customtkinter as ctk
+import serial
+import json
 
+bt_serial = serial.Serial("COM1", 115200, timeout=2)
+
+score = 0
+highest_record = 0
+highest_record_name = ""
+nameList = [""]
+scoreList = [0]
+
+window = ctk.CTk()
+window.title("Paddle Software")
+window.geometry("1920x1080")
+
+window.mainloop()
