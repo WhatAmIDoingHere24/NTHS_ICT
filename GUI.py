1ddbe90870f70764858459c766eb2a7bda5692ac
WhatAmIDoingHere24 <106035602+WhatAmIDoingHere24@users.noreply.github.com>
2025-04-01 21:21:05 -0700

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -1,1 +1,27 @@
+import customtkinter
+import _json
+import simplepyble
+
+
+highestRecord = 30
+highestRecordName = "jett"
+
+customtkinter.set_appearance_mode("System")  # Modes: system (default), light, dark
+customtkinter.set_default_color_theme("blue")  # Themes: blue (default), dark-blue, green
+
+app = customtkinter.CTk()  # create CTk window like you do with the Tk window
+app.geometry("500x500")
+
+
+
+label = customtkinter.CTkLabel(app, text=f"Current Highest Record is: {highestRecord} | this was acomplished by: {highestRecordName}", fg_color="brown")
+progressbar = customtkinter.CTkProgressBar(app, height=300,orientation="vertical",width=40,fg_color="blue",progress_color="green",mode='determinate')
+
+progressbar.set(float(f"0.{highestRecord}"))
+
+label.grid(column=0,row=0)
+progressbar.grid(column=25,row=8)
+
+app.mainloop()
+
 
